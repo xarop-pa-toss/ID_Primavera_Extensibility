20da721c3b0af6f08c0a036c3c2ee1ac0d4e9af7
xarop-pa-toss <rpgdsantos@gmail.com>
2023-08-07 16:27:13 +0100

    Criação dos forms VendArtigo e VendFornecedor e OpenWForm.cs com funções para abrir os forms no Primavera

--- ASSREG-Faturacao/Sales/OpenFormCode.cs
+++ ASSREG-Faturacao/Sales/OpenFormCode.cs
@@ -1,10 +1,4 @@
-﻿using Primavera.Extensibility.BusinessEntities;
-using Primavera.Extensibility.CustomCode;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Primavera.Extensibility.CustomCode;
 
 namespace ASRLB_ImportacaoFatura.Sales
 {
---
+++ FRUTI_Extens/OpenWForm.cs
@@ -0,0 +1,20 @@
+﻿using Primavera.Extensibility.CustomCode;
+
+namespace FRUTI_Extens.Sales
+{
+    public class OpenWForm : CustomCode
+    {
+        public void Abrir_VendArtigo_WF()
+        {
+            VendArtigo form = new VendArtigo();
+            form.ShowDialog();
+            PSO.UI.AdicionaFormMDI(form);
+        }
+        public void Abrir_VendFornecedor_WF()
+        {
+            VendFornecedor form = new VendFornecedor();
+            form.ShowDialog();
+            PSO.UI.AdicionaFormMDI(form);
+        }
+    }
+}
