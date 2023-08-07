fe7d15e9b2d550711a331a2c5c8f01d456c3d98b
xarop-pa-toss <rpgdsantos@gmail.com>
2023-08-07 15:58:54 +0100

    Convertido EditorVendas

--- FRUTI_Extens/Sales/UiEditorVendas.cs
+++ FRUTI_Extens/Sales/UiEditorVendas.cs
@@ -5,12 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using StdBE100; using StdPlatBS100; using ErpBS100;
+using StdBE100;
+using StdPlatBS100;
+using ErpBS100;
+using BasBE100;
+using Primavera.Extensibility.BusinessEntities.ExtensibilityService.EventArgs;
 
 
 namespace FRUTI_Extens.Sales
 {
     public class UiEditorVendas : EditorVendas
     {
+        public override void AntesDeGravar(ref bool Cancel, ExtensibilityEventArgs e)
+        {
+            if (DocumentoVenda.TotalDocumento == 0) {
+                PSO.MensagensDialogos.MostraAviso("Documento com valor 0 (Zero)", StdBSTipos.IconId.PRI_Informativo);
+                Cancel = true;
+            }
+
+            base.AntesDeGravar(ref Cancel, e);
+        }
     }
 }
