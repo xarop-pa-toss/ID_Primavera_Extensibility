71dc8b1760ee9ba7b80260f17fa8e920eed16f29
xarop-pa-toss <rpgdsantos@gmail.com>
2023-09-13 18:29:34 +0100

    Continuação de conversão do código CLCC

--- CLCC_Extens/FormGeraFatura_WF.cs
+++ CLCC_Extens/FormGeraFatura_WF.cs
@@ -1,4 +1,4 @@
-﻿using System; using System.IO;
+﻿using System; using System.IO; using System.Collections.Generic;
 using System.Windows.Forms;
 using ErpBS100; using StdPlatBS100;
 
@@ -8,7 +8,8 @@ namespace CLCC_Extens
     {
         private ErpBS _BSO;
         private StdPlatBS _PSO;
-        internal string _pathCompletoOriginal, _pathCompletoNovo, _nomeNovo;
+        private string _pathCompletoOriginal, _pathCompletoNovo, _nomeNovo;
+        private bool _ficheiroComErros;
 
         public FormGeraFatura_WF()
         {
@@ -56,61 +57,88 @@ namespace CLCC_Extens
                 return;
             }
             #endregion
-            #region Criação de ficheiro temporário
-
-            _nomeNovo = $"{DateTime.Now:ddMMyyHHmm}.txt";
-            _pathCompletoNovo = $"C:\\TEMP\\{_nomeNovo}";
-
-            #endregion
 
             listBox_Output.Items.Clear();
             listBox_Output.Items.Add("A copiar ficheiro...");
 
+            #region Criação de ficheiro temporário
+            _nomeNovo = $"{DateTime.Now:ddMMyyHHmm}.txt";
+            _pathCompletoNovo = $"C:\\TEMP\\{_nomeNovo}";
+            #endregion
 
+            listBox_Output.Items.Add($"Ficheiro copiado para {_pathCompletoNovo}");
+            listBox_Output.Items.Add("A ler informação do ficheiro...");
+            try {
+                List<string> linhasLista = new List<string>(File.ReadAllLines(_pathCompletoNovo));
+            }
+            catch {
+                listBox_Output.Items.Add("Erro ao ler linhas do ficheiro. Não foi gerado nenhum documento.");
+                _PSO.MensagensDialogos.MostraAviso("Erro ao ler linhas do ficheiro. Não foi gerado nenhum documento.", StdBSTipos.IconId.PRI_Critico);
+                return;
+            }
 
-FileCopy strNomeCompleto, strNovoNomeCompleto
-List1.AddItem "Cópia criada. Novo ficheiro: " & strNovoNomeCompleto
-List1.AddItem "A ler a informação do ficheiro ... "
+            listBox_Output.Items.Add("A validar linhas");
 
-LER_ParaArray(strNovoNomeCompleto)
-
-If ExistemErros Then
-
-    For i = 1 To UBound(ArrErros)
-        List1.AddItem ArrErros(i)
-    Next i
-    List1.AddItem "OPERAÇÃO CANCELADA. OCORRERAM ERROS!"
-    Exit Sub
-
-
-End If
-
-'DoEvents
-Res = Processar()
-If Res<> "" Then
-    List1.AddItem Res
-Else
-    List1.AddItem "OPERAÇÃO REALIZADA COM EXITO!"
-End If
-
-Kill strNovoNomeCompleto
-
-Exit Sub
-
-erro:
-            If Err.Number = 70 Then
-                MsgBox "OPERAÇÃO CANCELADA. O FICHEIRO DEVE ESTAR FECHADO!", vbInformation + vbOKOnly, "Informação"
-    Else
-        MsgBox Err.Number & Err.Description, , "Informação"
-        List1.AddItem "OPERAÇÃO CANCELADA. OCORRERAM ERROS!"
-        Close NumFich
-        Kill strNovoNomeCompleto
-    End If
         }
 
-        private void btn_Cancelar_Click(object sender, EventArgs e)
+        private List<string> ValidarLinhas(string path)
         {
+            
+            _ficheiroComErros = false;
 
+FNUM = FreeFile
+Open strFich For Input Access Read As #FNUM  'Input é para ler
+NumFich = FNUM
+i = 0
+k = 0
+
+List1.AddItem "Lendo ficheiro, aguarde por favor"
+
+Do Until EOF(FNUM)
+    i = i + 1
+
+    List1.AddItem "Linha " & i
+
+    ReDim Preserve arrLinhas(1 To i)
+    Line Input #FNUM, arrLinhas(i)
+    
+    If Mid(arrLinhas(i), 1, 3) = "***" Then
+        'Ignora, não faz nada
+    ElseIf UCase(Trim(Mid(arrLinhas(i), 1, 2))) = "TC" And jaVerificado = False Then
+
+
+        'É uma linha de Factura
+        jaVerificado = True
+        If Aplicacao.BSO.Comercial.Artigos.Existe("TC") = False Then
+            k = k + 1
+            ReDim Preserve ArrErros(1 To k)
+            ArrErros(k) = "Erro: Produto inexistente - TC"
+            ExistemErros = True
+        End If
+    ElseIf UCase(Trim(Mid(arrLinhas(i), 1, 2))) <> "TC" And Mid(arrLinhas(i), 1, 2) <> "***" Then
+        'É o cliente
+        If Aplicacao.BSO.Comercial.Clientes.Existe(Trim(Split(arrLinhas(i), ",")(0))) = False Then
+            k = k + 1
+            ReDim Preserve ArrErros(1 To k)
+            ArrErros(k) = "Erro linha " & i & ". Entidade inexistente - " & Trim(Split(arrLinhas(i), ",")(0)) & ""
+            ExistemErros = True
+
+
+        End If
+
+
+        If Aplicacao.BSO.Comercial.CondsPagamento.Existe(Trim(Split(arrLinhas(i), ",")(1))) = False Then
+            k = k + 1
+            ReDim Preserve ArrErros(1 To k)
+            ArrErros(k) = "Erro linha " & i & ". Condição de Pagamento inexistente - " & Trim(Split(arrLinhas(i), ",")(1)) & ""
+            ExistemErros = True
+        End If
+
+
+    End If
+Loop
+
+Close FNUM
         }
     }
 }
