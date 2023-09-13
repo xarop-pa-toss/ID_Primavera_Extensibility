a3e46158e03b522566f4881fb09db41304b3fdf3
xarop-pa-toss <rpgdsantos@gmail.com>
2023-09-13 15:47:20 +0100

    Criação de CLCC_Extens. Para usar WIndowsForms por isso foi importado de outro projecto o código necessário (Motores e OpenWForm.cs)

---
+++ CLCC_Extens/Sales/UiEditorVendas.cs
@@ -0,0 +1,16 @@
+using Primavera.Extensibility.Sales.Editors;
+using Primavera.Extensibility.BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CLCC_Extens.Sales
+{
+    public class UiEditorVendas : EditorVendas
+    {
+    }
+}
