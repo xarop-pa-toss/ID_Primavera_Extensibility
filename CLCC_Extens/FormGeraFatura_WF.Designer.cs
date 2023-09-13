7b2d606d920a74cc5d581e593b41c601b7b2d7a3
xarop-pa-toss <rpgdsantos@gmail.com>
2023-09-13 17:52:26 +0100

    Criado form para CLCC_Extens Conversão de algum código do form

--- CLCC_Extens/FormGeraFatura_WF.Designer.cs
+++ CLCC_Extens/FormGeraFatura_WF.Designer.cs
@@ -30,8 +30,11 @@ namespace CLCC_Extens
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGeraFatura_WF));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtbox_Ficheiro = new System.Windows.Forms.TextBox();
+            this.btn_EscolherFicheiro = new System.Windows.Forms.Button();
+            this.listBox_Output = new System.Windows.Forms.ListBox();
+            this.btn_Iniciar = new System.Windows.Forms.Button();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -43,33 +46,66 @@ namespace CLCC_Extens
             this.label1.TabIndex = 0;
             this.label1.Text = "Ficheiro:";
             // 
-            // textBox1
+            // txtbox_Ficheiro
             // 
-            this.textBox1.Location = new System.Drawing.Point(65, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(295, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtbox_Ficheiro.Location = new System.Drawing.Point(65, 21);
+            this.txtbox_Ficheiro.Name = "txtbox_Ficheiro";
+            this.txtbox_Ficheiro.Size = new System.Drawing.Size(295, 20);
+            this.txtbox_Ficheiro.TabIndex = 1;
             // 
-            // button1
+            // btn_EscolherFicheiro
             // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button1.Location = new System.Drawing.Point(366, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(28, 25);
-            this.button1.TabIndex = 2;
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_EscolherFicheiro.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_EscolherFicheiro.BackgroundImage")));
+            this.btn_EscolherFicheiro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_EscolherFicheiro.Location = new System.Drawing.Point(366, 19);
+            this.btn_EscolherFicheiro.Name = "btn_EscolherFicheiro";
+            this.btn_EscolherFicheiro.Size = new System.Drawing.Size(28, 25);
+            this.btn_EscolherFicheiro.TabIndex = 2;
+            this.btn_EscolherFicheiro.UseVisualStyleBackColor = true;
+            this.btn_EscolherFicheiro.Click += new System.EventHandler(this.btn_EscolherFicheiro_Click);
+            // 
+            // listBox_Output
+            // 
+            this.listBox_Output.FormattingEnabled = true;
+            this.listBox_Output.Location = new System.Drawing.Point(14, 55);
+            this.listBox_Output.Name = "listBox_Output";
+            this.listBox_Output.Size = new System.Drawing.Size(773, 355);
+            this.listBox_Output.TabIndex = 3;
+            // 
+            // btn_Iniciar
+            // 
+            this.btn_Iniciar.Location = new System.Drawing.Point(620, 419);
+            this.btn_Iniciar.Name = "btn_Iniciar";
+            this.btn_Iniciar.Size = new System.Drawing.Size(75, 28);
+            this.btn_Iniciar.TabIndex = 4;
+            this.btn_Iniciar.Text = "Iniciar";
+            this.btn_Iniciar.UseVisualStyleBackColor = true;
+            this.btn_Iniciar.Click += new System.EventHandler(this.btn_Iniciar_Click);
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Location = new System.Drawing.Point(713, 419);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(75, 28);
+            this.btn_Cancelar.TabIndex = 5;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
             // 
             // FormGeraFatura_WF
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(800, 456);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.btn_Iniciar);
+            this.Controls.Add(this.listBox_Output);
+            this.Controls.Add(this.btn_EscolherFicheiro);
+            this.Controls.Add(this.txtbox_Ficheiro);
             this.Controls.Add(this.label1);
             this.Name = "FormGeraFatura_WF";
-            this.Text = "FormGeraFatura_WF";
+            this.Text = "Gera Faturas";
+            this.Load += new System.EventHandler(this.FormGeraFatura_WF_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,7 +114,10 @@ namespace CLCC_Extens
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtbox_Ficheiro;
+        private System.Windows.Forms.Button btn_EscolherFicheiro;
+        private System.Windows.Forms.ListBox listBox_Output;
+        private System.Windows.Forms.Button btn_Iniciar;
+        private System.Windows.Forms.Button btn_Cancelar;
     }
 }
--- CLCC_Extens/FormGeraFatura_WF.cs
+++ CLCC_Extens/FormGeraFatura_WF.cs
@@ -1,20 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; using System.IO;
 using System.Windows.Forms;
+using ErpBS100; using StdPlatBS100;
 
 namespace CLCC_Extens
 {
     public partial class FormGeraFatura_WF : Form
     {
+        private ErpBS _BSO;
+        private StdPlatBS _PSO;
+        internal string _pathCompletoOriginal, _pathCompletoNovo, _nomeNovo;
+
         public FormGeraFatura_WF()
         {
             InitializeComponent();
+        }
+
+        private void FormGeraFatura_WF_Load(object sender, EventArgs e)
+        {
+            Motor.PriEngine.CreateContext("CLCC", "id", "*Pelicano*");
+            _BSO = Motor.PriEngine.Engine;
+            _PSO = Motor.PriEngine.Platform;
+
+            listBox_Output.Items.Clear();
+            txtbox_Ficheiro.Clear();
+        }
+
+        private void btn_EscolherFicheiro_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) 
+            {
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "Todos os ficheiros (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    _pathCompletoOriginal = openFileDialog.FileName;
+                    txtbox_Ficheiro.Text = _pathCompletoOriginal;
+                }
+            }
+        }
+
+        private void btn_Iniciar_Click(object sender, EventArgs e)
+        {
+            int i;
+            string res;
+
+            #region Validação de existência do ficheiro
+            if (string.IsNullOrEmpty(txtbox_Ficheiro.Text)) {
+                _PSO.MensagensDialogos.MostraAviso("O caminho do ficheiro está vazio.", StdBSTipos.IconId.PRI_Exclama);
+                return;
+            }
+
+            if (!File.Exists(_pathCompletoOriginal)) {
+                _PSO.MensagensDialogos.MostraAviso("O ficheiro não existe no caminho especificado.", StdBSTipos.IconId.PRI_Exclama);
+                return;
+            }
+            #endregion
+            #region Criação de ficheiro temporário
+
+            _nomeNovo = $"{DateTime.Now:ddMMyyHHmm}.txt";
+            _pathCompletoNovo = $"C:\\TEMP\\{_nomeNovo}";
+
+            #endregion
+
+            listBox_Output.Items.Clear();
+            listBox_Output.Items.Add("A copiar ficheiro...");
+
+
+
+FileCopy strNomeCompleto, strNovoNomeCompleto
+List1.AddItem "Cópia criada. Novo ficheiro: " & strNovoNomeCompleto
+List1.AddItem "A ler a informação do ficheiro ... "
+
+LER_ParaArray(strNovoNomeCompleto)
+
+If ExistemErros Then
+
+    For i = 1 To UBound(ArrErros)
+        List1.AddItem ArrErros(i)
+    Next i
+    List1.AddItem "OPERAÇÃO CANCELADA. OCORRERAM ERROS!"
+    Exit Sub
+
+
+End If
+
+'DoEvents
+Res = Processar()
+If Res<> "" Then
+    List1.AddItem Res
+Else
+    List1.AddItem "OPERAÇÃO REALIZADA COM EXITO!"
+End If
+
+Kill strNovoNomeCompleto
+
+Exit Sub
+
+erro:
+            If Err.Number = 70 Then
+                MsgBox "OPERAÇÃO CANCELADA. O FICHEIRO DEVE ESTAR FECHADO!", vbInformation + vbOKOnly, "Informação"
+    Else
+        MsgBox Err.Number & Err.Description, , "Informação"
+        List1.AddItem "OPERAÇÃO CANCELADA. OCORRERAM ERROS!"
+        Close NumFich
+        Kill strNovoNomeCompleto
+    End If
+        }
+
+        private void btn_Cancelar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
