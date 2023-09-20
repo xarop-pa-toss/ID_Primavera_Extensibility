db8de8e3f63578b3eff93c428c73b9bb5fc45015
xarop-pa-toss <rpgdsantos@gmail.com>
2023-09-20 17:15:33 +0100

    Corrigida Query07 e Query08

--- PP_Extens/PP_PPCS/Purchases/ImportaDocs.cs
+++ PP_Extens/PP_PPCS/Purchases/ImportaDocs.cs
@@ -457,7 +457,7 @@ namespace PP_PPCS
                             }
 
                             // Verificar se o documento original é com iva incluido
-                            ivaIncluido = _BSO.Consulta(QueriesSQL.GetQuery08(TipoDoc, Serie, NumDoc.ToString())).Valor("ivaincluido");
+                            ivaIncluido = _BSO.Consulta(QueriesSQL.GetQuery08(TipoDoc, Serie)).Valor("IvaIncluido");
                             //
                             RSet = _BSO.Consulta(QueriesSQL.GetQuery09(Filial, TipoDoc, Serie, NumDoc.ToString()));
 
