02dd4ed00cebbd283b9ec5b5120ff40a03933df6
xarop-pa-toss <rpgdsantos@gmail.com>
2023-12-07 15:11:33 +0000

    Feito InputForm para usar em vez do do Primavera. Por testar mas ajusta a altura de acordo com a altura da descrição

---
+++ PP_Extens/PP_Extens/InputForm.cs
@@ -0,0 +1,28 @@
+﻿using Primavera.Extensibility.BusinessEntities;
+using Primavera.Extensibility.CustomForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Extens
+{
+    public partial class InputForm : CustomForm
+    {
+        private string _descricao;
+
+        public InputForm(string descricao)
+        {
+            _descricao = descricao;
+            InitializeComponent();
+        }
+
+        private void InputForm_Load(object sender, EventArgs e)
+        {
+            // Ajustar as altura do form à altura da descrição
+            this.Height += lbl_Descricao.Height;
+        }
+    }
+}
