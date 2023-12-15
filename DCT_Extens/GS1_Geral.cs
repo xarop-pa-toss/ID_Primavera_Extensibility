e5e14affe43576238d42dabe4707751d7689bedc
xarop-pa-toss <rpgdsantos@gmail.com>
2023-12-15 11:16:59 +0000

    Continua conversão de código GS1 pra DCT_Extens

--- DCT_Extens/GS1_Geral.cs
+++ DCT_Extens/GS1_Geral.cs
@@ -11,6 +11,7 @@ using ErpBS100;
 using StdPlatBS100;
 using StdBE100;
 using BasBE100;
+using System.Drawing.Text;
 
 namespace DCT_Extens
 {
@@ -26,6 +27,7 @@ namespace DCT_Extens
 
         private const string PREFIXO = "3";
         private const string PREFIXO_EMPRESA = "560089876";
+        HelperFunctions Helpers = new HelperFunctions();
 
         public GS1_Geral(string Filial, string Serie, string Tipo, long NumDoc, VndBEDocumentoVenda dv, ErpBS BSO, StdBSInterfPub PSO)
         {
@@ -36,9 +38,12 @@ namespace DCT_Extens
             _dv = dv;
             _BSO = BSO;
             _PSO = PSO;
+        }
 
+        public void EditorVendas_AntesDeGravar()
+        {
             string strCliente = _dv.Entidade;
-            BasBECliente objCliente= BSO.Base.Clientes.Consulta(strCliente);
+            BasBECliente objCliente = _BSO.Base.Clientes.Consulta(strCliente);
 
             if ((bool)objCliente.CamposUtil["CDU_SSCC"].Valor)
             {
@@ -47,13 +52,57 @@ namespace DCT_Extens
                 sql.tpQuery = StdBETipos.EnumTpQuery.tpUPDATE;
                 sql.Tabela = "TDU_TTE_PackingCodes";
                 sql.AddCampo("IDENTITY_INSERT", "ON");
+                _PSO.ExecSql.Executa(sql);
+                sql.Dispose();
 
-                PSO.ExecSql.Executa(sql);
+                // Calcular digito controlo para cada linha de artigo e criar a sequencia final.
+                // Inserir a sequencia final em cada linha e na tabela TDU_TTE_PackingCodes
+                foreach (VndBELinhaDocumentoVenda linha in _dv.Linhas)
+                {
+                    if (linha.TipoLinha.Equals(10))
+                    {
+                        // Incrementar número de sequência da TDU_TTE_PackingCodes (última sequência inserida na tabela)
+                        string sequencia = GetSequencia() + 1;
 
+                        // Concatena todos os elementos necessários para calcular o Digito de Controlo.
+                        // Por fim, concatena o digito ao restante.
+                        string strFinal = PREFIXO + PREFIXO_EMPRESA + sequencia;
+                        strFinal += GetDigitoControlo(strFinal);
 
+                        // Cada linha com artigo no DocVenda tem de ter a strFinal no seu CDU_SSCC
+                        sql.tpQuery = StdBETipos.EnumTpQuery.tpUPDATE;
+                        sql.Tabela = "LinhasDoc";
+                        sql.AddCampo("CDU_SSCC", strFinal);
+                        sql.AddCampo("ID", linha.IdLinha, true);
+                        _PSO.ExecSql.Executa(sql);
+                        sql.Dispose();
+
+                        // Para cada linha que recebe um strFinal, tem de ser criada uma entrada na tabela TDU_TTE_PackingCodes
+                        string strComPrefixo = "(00)" + strFinal;
+                        Dictionary<string, string> dict = new Dictionary<string, string>
+                        {
+                            { "IdCabec", _dv.ID },
+                            { "IdLinha", linha.IdLinha },
+                            { "PalletCode", strComPrefixo },
+                            { "Sequencia", sequencia }
+                        };
+                        Helpers.TDU_Insert("TDU_TTE_PackingCodes", dict, _BSO);
+                    }
+                }
             }
         }
 
+        private string GetSequencia()
+        {
+
+        }
+
+        private int GetDigitoControlo(string strFinal)
+        {
+
+        }
+
     }
-    
-}
+} 
+
+ 
---
+++ DCT_Extens/HelperFunctions.cs
@@ -0,0 +1,40 @@
+﻿using Primavera.Extensibility;
+using Primavera.Extensibility.BusinessEntities;
+using Primavera.Extensibility.CustomCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErpBS100; using StdPlatBS100; using BasBE100; using StdBE100;
+
+
+namespace DCT_Extens
+{
+    public class HelperFunctions : CustomCode
+    {
+        public void TDU_Insert(string NomeTDU, Dictionary<string, string> Dict, ErpBS BSO)
+        {
+            // Criar Campos para RegistoUtil para então inserir na TDU
+            // https://v10api.primaverabss.com/html/api/plataforma/StdBE100.StdBETipos.EnumTipoCampo.html
+
+            StdBERegistoUtil registoUtil = new StdBERegistoUtil();
+            StdBECampos linha = new StdBECampos();
+
+            foreach (KeyValuePair<string, string> kvp in Dict)
+            {
+                StdBECampo campo = new StdBECampo();
+                campo.Nome = kvp.Key;
+                campo.Valor = kvp.Value;
+
+                linha.Add(campo);
+            }
+            registoUtil.EmModoEdicao = true;
+            registoUtil.Campos = linha;
+            registoUtil.EmModoEdicao = false;
+
+            BSO.TabelasUtilizador.Actualiza(NomeTDU, registoUtil);
+        }
+    }
+}
+
