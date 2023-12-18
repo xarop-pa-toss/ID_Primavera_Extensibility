1d1013aee1b2803c410bd276548e5677b2fb6921
xarop-pa-toss <rpgdsantos@gmail.com>
2023-12-18 18:08:55 +0000

    // Carregar "arrTDU" e "arrSerie". Tem de ser feito pois TipoDocumentoIdentificado não dá série e não existe override para Série identificada

--- DCT_Extens/Internal/UiEditorInternos.cs
+++ DCT_Extens/Internal/UiEditorInternos.cs
@@ -10,6 +10,7 @@ using StdBE100; using VndBE100; using ErpBS100; using StdBE100; using BasBE100;
 using DCT_Extens.Helpers;
 using System.Data;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DCT_Extens.Internal
 {
@@ -23,15 +24,33 @@ namespace DCT_Extens.Internal
         {
             base.TipoDocumentoIdentificado(TipoDocumento, ref Cancel, e);
 
-            // Carregar "arrTDU" e "arrSerie"
+            // Carregar "arrTDU" e "arrSerie". Tem de ser feito pois TipoDocumentoIdentificado não dá série e não existe override para Série identificada
             _tabelaOperadores = _Helpers.GetTabela("SELECT * FROM TDU_OperadorQuebra;");
             _tabelaSeries = _Helpers.GetTabela($"" +
-                $"SELECT TipoDoc, Serie, CDU_PedeOperador_Motivo, CDU_PedeOperador_Operador " +
-                $"FROM SeriesStocks " +
-                $"WHERE DataInicial >= '2022-01-01' " +
-                $"AND (CDU_PedeOperador_Motivo = 1 OR CDU_PedeOperador_Operador = 1);");
+                $" SELECT TipoDoc, Serie, CDU_PedeOperador_Motivo, CDU_PedeOperador_Operador " +
+                $" FROM SeriesStocks " +
+                $" WHERE " +
+                $"  DataInicial >= '2022-01-01' " +
+                $"   AND (CDU_PedeOperador_Motivo = 1 OR CDU_PedeOperador_Operador = 1)" +
+                $"   AND TipoDoc = '{TipoDocumento}'" +
+                $"   AND Serie = '{};");
 
-            // Validação das séries
+            
+            bool GetCDU_PedeOperadorPorSerie = false;
+            foreach (DataRow linha in _tabelaSeries.Rows)
+            {
+
+
+
+    //            For i = LBound(arrSerie, 2) To UBound(arrSerie, 2)
+    //    If arrSerie(0, i) = TipoDoc And arrSerie(1, i) = Serie And(arrSerie(2, i) = True Or arrSerie(3, i) = True) Then
+    //        pedeMotivo = arrSerie(2, i)
+    //        pedeOperador = arrSerie(3, i)
+    //        ValidarSerie = True
+    //        Exit For
+    //    End If
+    //Next i
+            }
 
             serieValida = ValidarSerie(Me.DocumentoStock.TipoDoc, Me.DocumentoStock.Serie)
 
