5d472282cb203e536eec00598dcfb77e3068b05d
xarop-pa-toss <rpgdsantos@gmail.com>
2023-12-20 17:30:12 +0000

    Criados alguns dos métodos do FormReimpressao

--- DCT_Extens/Forms/FormReimpressao.Designer.cs
+++ DCT_Extens/Forms/FormReimpressao.Designer.cs
@@ -45,7 +45,18 @@ namespace DCT_Extens.Forms
             this.dtPicker_DataDocInicial = new System.Windows.Forms.DateTimePicker();
             this.dtPicker_DataDocFinal = new System.Windows.Forms.DateTimePicker();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBox_Mapas = new System.Windows.Forms.ComboBox();
+            this.numUpDown_NumVias = new System.Windows.Forms.NumericUpDown();
+            this.f4_Cliente = new PRISDK100.F4();
+            this.priGrelha_Docs = new PRISDK100.PriGrelha();
+            this.btn_SeleccionarTodos = new System.Windows.Forms.Button();
+            this.btn_LimparSeleccao = new System.Windows.Forms.Button();
+            this.listBox_TipoDoc = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_NumVias)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priGrelha_Docs)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_Actualizar
@@ -54,19 +65,22 @@ namespace DCT_Extens.Forms
             this.btn_Actualizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_Actualizar.Location = new System.Drawing.Point(10, 10);
             this.btn_Actualizar.Name = "btn_Actualizar";
-            this.btn_Actualizar.Size = new System.Drawing.Size(84, 27);
+            this.btn_Actualizar.Size = new System.Drawing.Size(80, 27);
             this.btn_Actualizar.TabIndex = 0;
-            this.btn_Actualizar.Text = "Button1";
+            this.btn_Actualizar.Text = "Actualizar";
+            this.btn_Actualizar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Actualizar.Click += new System.EventHandler(this.btn_Actualizar_Click);
             // 
             // btn_Imprimir
             // 
             this.btn_Imprimir.Image = ((System.Drawing.Image)(resources.GetObject("btn_Imprimir.Image")));
             this.btn_Imprimir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_Imprimir.Location = new System.Drawing.Point(100, 10);
+            this.btn_Imprimir.Location = new System.Drawing.Point(96, 10);
             this.btn_Imprimir.Name = "btn_Imprimir";
-            this.btn_Imprimir.Size = new System.Drawing.Size(84, 27);
+            this.btn_Imprimir.Size = new System.Drawing.Size(80, 27);
             this.btn_Imprimir.TabIndex = 1;
-            this.btn_Imprimir.Text = "Imprimir";
+            this.btn_Imprimir.Text = "Imprimir  ";
+            this.btn_Imprimir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // labelInicial
             // 
@@ -108,28 +122,194 @@ namespace DCT_Extens.Forms
             this.groupBox1.Controls.Add(this.dtPicker_DataDocInicial);
             this.groupBox1.Location = new System.Drawing.Point(10, 55);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(163, 92);
+            this.groupBox1.Size = new System.Drawing.Size(152, 92);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Data dos Documentos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mapa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nº Vias:";
+            // 
+            // cmbBox_Mapas
+            // 
+            this.cmbBox_Mapas.FormattingEnabled = true;
+            this.cmbBox_Mapas.Location = new System.Drawing.Point(223, 66);
+            this.cmbBox_Mapas.Name = "cmbBox_Mapas";
+            this.cmbBox_Mapas.Size = new System.Drawing.Size(203, 21);
+            this.cmbBox_Mapas.TabIndex = 6;
+            this.cmbBox_Mapas.TextChanged += new System.EventHandler(this.cmbBox_Mapas_TextChanged);
+            // 
+            // numUpDown_NumVias
+            // 
+            this.numUpDown_NumVias.Location = new System.Drawing.Point(223, 102);
+            this.numUpDown_NumVias.Name = "numUpDown_NumVias";
+            this.numUpDown_NumVias.Size = new System.Drawing.Size(37, 20);
+            this.numUpDown_NumVias.TabIndex = 7;
+            this.numUpDown_NumVias.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // f4_Cliente
+            // 
+            this.f4_Cliente.AgrupaOutrosTerceiros = false;
+            this.f4_Cliente.Audit = "mnuTabClientes";
+            this.f4_Cliente.AutoComplete = false;
+            this.f4_Cliente.BackColorLocked = System.Drawing.SystemColors.ButtonFace;
+            this.f4_Cliente.CampoChave = "Cliente";
+            this.f4_Cliente.CampoChaveFisica = "";
+            this.f4_Cliente.CampoDescricao = "Nome";
+            this.f4_Cliente.Caption = "Cliente:";
+            this.f4_Cliente.CarregarValoresEdicao = false;
+            this.f4_Cliente.Categoria = PRISDK100.clsSDKTypes.EnumCategoria.Clientes;
+            this.f4_Cliente.ChaveFisica = "";
+            this.f4_Cliente.ChaveNumerica = false;
+            this.f4_Cliente.F4Modal = false;
+            this.f4_Cliente.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.f4_Cliente.IDCategoria = "Clientes";
+            this.f4_Cliente.Location = new System.Drawing.Point(10, 163);
+            this.f4_Cliente.MaxLengthDescricao = 0;
+            this.f4_Cliente.MaxLengthF4 = 50;
+            this.f4_Cliente.MinimumSize = new System.Drawing.Size(37, 21);
+            this.f4_Cliente.Modulo = "BAS";
+            this.f4_Cliente.MostraDescricao = true;
+            this.f4_Cliente.MostraLink = true;
+            this.f4_Cliente.Name = "f4_Cliente";
+            this.f4_Cliente.PainesInformacaoRelacionada = false;
+            this.f4_Cliente.PainesInformacaoRelacionadaMultiplasChaves = false;
+            this.f4_Cliente.PermiteDrillDown = true;
+            this.f4_Cliente.PermiteEnabledLink = true;
+            this.f4_Cliente.PodeEditarDescricao = false;
+            this.f4_Cliente.ResourceID = 669;
+            this.f4_Cliente.ResourcePersonalizada = false;
+            this.f4_Cliente.Restricao = "";
+            this.f4_Cliente.SelectionFormula = "";
+            this.f4_Cliente.Size = new System.Drawing.Size(416, 21);
+            this.f4_Cliente.TabIndex = 8;
+            this.f4_Cliente.TextoDescricao = "";
+            this.f4_Cliente.WidthEspacamento = 60;
+            this.f4_Cliente.WidthF4 = 1100;
+            this.f4_Cliente.WidthLink = 800;
+            // 
+            // priGrelha_Docs
+            // 
+            this.priGrelha_Docs.BackColor = System.Drawing.Color.White;
+            this.priGrelha_Docs.BandaMenuContexto = "";
+            this.priGrelha_Docs.BotaoConfigurarActiveBar = true;
+            this.priGrelha_Docs.BotaoProcurarActiveBar = false;
+            this.priGrelha_Docs.CaminhoTemplateImpressao = "";
+            this.priGrelha_Docs.Cols = null;
+            this.priGrelha_Docs.ColsFrozen = -1;
+            this.priGrelha_Docs.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.priGrelha_Docs.Location = new System.Drawing.Point(18, 249);
+            this.priGrelha_Docs.Name = "priGrelha_Docs";
+            this.priGrelha_Docs.NumeroMaxRegistosSemPag = 150000;
+            this.priGrelha_Docs.NumeroRegistos = 0;
+            this.priGrelha_Docs.NumLinhasCabecalho = 1;
+            this.priGrelha_Docs.OrientacaoMapa = PRISDK100.clsSDKTypes.OrientacaoImpressao.oiDefault;
+            this.priGrelha_Docs.ParentFormModal = false;
+            this.priGrelha_Docs.PermiteActiveBar = false;
+            this.priGrelha_Docs.PermiteActualizar = true;
+            this.priGrelha_Docs.PermiteAgrupamentosUser = true;
+            this.priGrelha_Docs.PermiteConfigurarDetalhes = false;
+            this.priGrelha_Docs.PermiteContextoVazia = false;
+            this.priGrelha_Docs.PermiteDataFill = false;
+            this.priGrelha_Docs.PermiteDetalhes = true;
+            this.priGrelha_Docs.PermiteEdicao = false;
+            this.priGrelha_Docs.PermiteFiltros = true;
+            this.priGrelha_Docs.PermiteGrafico = true;
+            this.priGrelha_Docs.PermiteGrandeTotal = false;
+            this.priGrelha_Docs.PermiteOrdenacao = true;
+            this.priGrelha_Docs.PermitePaginacao = false;
+            this.priGrelha_Docs.PermiteScrollBars = true;
+            this.priGrelha_Docs.PermiteStatusBar = true;
+            this.priGrelha_Docs.PermiteVistas = true;
+            this.priGrelha_Docs.PosicionaColunaSeguinte = true;
+            this.priGrelha_Docs.Size = new System.Drawing.Size(625, 331);
+            this.priGrelha_Docs.TabIndex = 10;
+            this.priGrelha_Docs.TituloGrelha = "";
+            this.priGrelha_Docs.TituloMapa = "";
+            this.priGrelha_Docs.TypeNameLinha = "";
+            this.priGrelha_Docs.TypeNameLinhas = "";
+            // 
+            // btn_SeleccionarTodos
+            // 
+            this.btn_SeleccionarTodos.Image = ((System.Drawing.Image)(resources.GetObject("btn_SeleccionarTodos.Image")));
+            this.btn_SeleccionarTodos.Location = new System.Drawing.Point(18, 220);
+            this.btn_SeleccionarTodos.Name = "btn_SeleccionarTodos";
+            this.btn_SeleccionarTodos.Size = new System.Drawing.Size(25, 23);
+            this.btn_SeleccionarTodos.TabIndex = 11;
+            this.btn_SeleccionarTodos.UseVisualStyleBackColor = true;
+            // 
+            // btn_LimparSeleccao
+            // 
+            this.btn_LimparSeleccao.Image = ((System.Drawing.Image)(resources.GetObject("btn_LimparSeleccao.Image")));
+            this.btn_LimparSeleccao.Location = new System.Drawing.Point(49, 220);
+            this.btn_LimparSeleccao.Name = "btn_LimparSeleccao";
+            this.btn_LimparSeleccao.Size = new System.Drawing.Size(25, 23);
+            this.btn_LimparSeleccao.TabIndex = 12;
+            this.btn_LimparSeleccao.UseVisualStyleBackColor = true;
+            // 
+            // listBox_TipoDoc
+            // 
+            this.listBox_TipoDoc.FormattingEnabled = true;
+            this.listBox_TipoDoc.Location = new System.Drawing.Point(432, 64);
+            this.listBox_TipoDoc.Name = "listBox_TipoDoc";
+            this.listBox_TipoDoc.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox_TipoDoc.Size = new System.Drawing.Size(211, 173);
+            this.listBox_TipoDoc.TabIndex = 13;
             // 
             // FormReimpressao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listBox_TipoDoc);
+            this.Controls.Add(this.btn_LimparSeleccao);
+            this.Controls.Add(this.btn_SeleccionarTodos);
+            this.Controls.Add(this.priGrelha_Docs);
+            this.Controls.Add(this.f4_Cliente);
+            this.Controls.Add(this.numUpDown_NumVias);
+            this.Controls.Add(this.cmbBox_Mapas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_Actualizar);
             this.Controls.Add(this.btn_Imprimir);
             this.Name = "FormReimpressao";
-            this.Size = new System.Drawing.Size(500, 465);
+            this.Size = new System.Drawing.Size(654, 593);
             this.Text = "FormReimpressão";
+            this.Load += new System.EventHandler(this.FormReimpressao_Load);
             this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_NumVias)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priGrelha_Docs)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private GroupBox groupBox1;
+        private Label label1;
+        private Label label2;
+        private ComboBox cmbBox_Mapas;
+        private NumericUpDown numUpDown_NumVias;
+        private PRISDK100.F4 f4_Cliente;
+        private PRISDK100.PriGrelha priGrelha_Docs;
+        private Button btn_SeleccionarTodos;
+        private Button btn_LimparSeleccao;
+        private ListBox listBox_TipoDoc;
     }
 }
--- DCT_Extens/Forms/FormReimpressao.cs
+++ DCT_Extens/Forms/FormReimpressao.cs
@@ -4,16 +4,190 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BasBE100;
 using System.Threading.Tasks;
 using DCT_Extens.Helpers;
+using ErpBS100;
+using StdPlatBS100;
+using PRISDK100;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using StdBE100;
 
 namespace DCT_Extens.Forms
 {
     public partial class FormReimpressao : CustomForm
     {
+        private ErpBS _BSO { get; set; }
+        private StdPlatBS _PSO { get; set; }
+        private clsSDKContexto _SDKContexto { get; set; }
+        private HelperFunctions _Helpers { get; set; }
+
+        private bool _mapasVazio;
+
         public FormReimpressao()
         {
+            _BSO = PriMotores.Motor;
+            _PSO = PriMotores.Plataforma;
+            _SDKContexto = PriMotores.PriSDKContexto;
+            _Helpers = new HelperFunctions();
+
             InitializeComponent();
         }
+
+        private void FormReimpressao_Load(object sender, EventArgs e)
+        {
+            // Inicializar controlos Primavera
+            priGrelha_Docs.Inicializa(_SDKContexto);
+            f4_Cliente.Inicializa(_SDKContexto);
+
+            // Inicialização dos controlos
+            InicializaPriGrelha();
+            InicializaListBox_TipoDoc();
+            InicializaCmbBox_Mapas();
+            
+            dtPicker_DataDocInicial.Value = DateTime.Now;
+            dtPicker_DataDocFinal.Value = DateTime.Now;
+        }
+
+        private void btn_Actualizar_Click(object sender, EventArgs e)
+        {
+            InicializaPriGrelha();
+        }
+
+        private void cmbBox_Mapas_TextChanged(object sender, EventArgs e)
+        {
+            ActualizaCBoxSerie();
+        }
+
+
+        #region Funções de Inicialização
+        private void InicializaPriGrelha()
+        {
+            priGrelha_Docs.TituloGrelha = "DocsReimpressao";
+            priGrelha_Docs.PermiteActualizar = true;
+            priGrelha_Docs.PermiteAgrupamentosUser = true;
+            priGrelha_Docs.PermiteScrollBars = true;
+            priGrelha_Docs.PermiteVistas = false;
+            priGrelha_Docs.PermiteEdicao = false;
+            priGrelha_Docs.PermiteDataFill = false;
+            priGrelha_Docs.PermiteFiltros = true;
+            priGrelha_Docs.PermiteActiveBar = false;
+            priGrelha_Docs.PermiteContextoVazia = false;
+            priGrelha_Docs.PermiteOrdenacao = true;
+            
+            // Colunas da tabela de reimpressão
+            // Cf - CheckBox - defines whether it will print or not
+            // Data - Date - document issuance date
+            // Doc (DrillDown) - Str - DocType
+            // Serie - Str
+            // Numero (DrillDown) - Long/Int
+            // Tipo Entidade - Str
+            // Entidade (DrillDown) - Str
+            // Moeda - Str/Currency
+            // Total - Double/Float - total value of the Doc
+            // Imp - Symbol - whether it has been printed or not
+            
+            priGrelha_Docs.AddColKey(strColKey: "Cf", intTipo: 10, strTitulo: "Cf.", dblLargura: 3, blnMostraSempre: true, blnVisivel: true);
+            priGrelha_Docs.AddColKey(strColKey: "Data", intTipo: 5, strTitulo: "Data", dblLargura: 13, strCamposBaseDados: "Data", blnMostraSempre: true);
+            priGrelha_Docs.AddColKey(strColKey: "TipoDoc", intTipo: 5, strTitulo: "Doc", dblLargura: 7, strCamposBaseDados: "TipoDoc", blnDrillDown: true, blnMostraSempre: true);
+            priGrelha_Docs.AddColKey(strColKey: "Serie", intTipo: 5, strTitulo: "Serie", dblLargura: 7, strCamposBaseDados: "Serie", blnMostraSempre: true);
+            priGrelha_Docs.AddColKey(strColKey: "NumDoc", intTipo: 5, strTitulo: "Numero", dblLargura: 10, strCamposBaseDados: "NumDoc", blnDrillDown: true, blnMostraSempre: true);
+            priGrelha_Docs.AddColKey(strColKey: "TipoEntidade", intTipo: 5, strTitulo: "Tipo Entidade", dblLargura: 5, strCamposBaseDados: "TipoEntidade", blnMostraSempre: true);
+            priGrelha_Docs.AddColKey(strColKey: "Entidade", intTipo: 5, strTitulo: "Entidade", dblLargura: 10, strCamposBaseDados: "Entidade", blnDrillDown: true, blnMostraSempre: true);
+            priGrelha_Docs.AddColKey(strColKey: "Moeda", intTipo: 5, strTitulo: "Moeda", dblLargura: 7, strCamposBaseDados: "Moeda", blnDrillDown: true, blnMostraSempre: true);
+            priGrelha_Docs.AddColKey(strColKey: "TotalDocumento", intTipo: 2, strTitulo: "Total", dblLargura: 10, strCamposBaseDados: "TotalDocumento", blnMostraSempre: true);
+
+        }
+        private void InicializaListBox_TipoDoc()
+        {
+            listBox_TipoDoc.Items.Clear();
+
+            DataTable tipoDocsTabela = _Helpers.GetDataTableDeSQL($"" +
+                $" SELECT Documento + ' - ' + Descricao AS Doc" +
+                $" FROM DocumentosVenda" +
+                $" WHERE Inactivo = 0" +
+                $"  AND CDU_ModReimpDocs = 1" +
+                $" ORDER BY Documento DESC;");
+
+            // Get coluna Doc como List e depois converte pra Array ao introduzir na ListBox (não aceita List)
+            // Mais eficiente que loop pela Lista toda.
+            // As razões são interessantes (se bem que em datasets pequenos podem não fazer grande diferença.
+            // https://pastebin.com/T6CXVXtE
+            var tipoDocsValores = tipoDocsTabela.AsEnumerable()
+                .Select(linha => linha.Field<string>("Docs")).ToList();
+
+            listBox_TipoDoc.Items.AddRange(tipoDocsValores.ToArray());
+        }
+        private void InicializaCmbBox_Mapas()
+        {
+            DataTable mapasTabela = _Helpers.GetDataTableDeSQL(
+                " SELECT Descricao " +
+                " FROM [PRIEMPRE].[dbo].[Mapas] " +
+                " WHERE " +
+                "   Categoria = 'DocVenda' " +
+                "   AND Apl = 'GCP' " +
+                "   AND Custom = 1 " +
+                "   ORDER BY Descricao;");
+
+            var tipoDocsLista = mapasTabela.AsEnumerable()
+                .Select(linha => linha.Field<string>("Descricao")).ToList();
+
+            if (!tipoDocsLista.Any())
+            {
+                PSO.MensagensDialogos.MostraErro("Não foi possivel encontrar mapas personalizados.");
+                Close();
+            }
+            else
+            {
+                cmbBox_Mapas.Items.Clear();
+                cmbBox_Mapas.Items.AddRange(tipoDocsLista.ToArray());
+            }
+
+
+        }
+        #endregion
+
+        #region Funções de Actualização
+        private void ActualizaPriGrelha()
+        {
+            DateTime dataInicial = dtPicker_DataDocInicial.Value;
+            DateTime dataFinal = dtPicker_DataDocFinal.Value;
+
+            if (listBox_TipoDoc.SelectedItems.Count.Equals(0))
+            {
+                PSO.MensagensDialogos.MostraAviso("Não foi seleccionado um tipo de documento.", StdBSTipos.IconId.PRI_Exclama);
+                return;
+            }
+
+            // Lista de strings com os valores seleccionados na ListBox para a query SQL. Fica apenas com o TipoDoc sem o hífen e descrição
+            List<string> tipoDocSelec = listBox_TipoDoc.SelectedItems
+                .Cast<string>()
+                .Select(x => x.Split(' ')[0].Trim() + ",")
+                .ToList();
+
+            // Remoção da virgula do último TipoDoc na lista para evitar erro de SQL e criação da string final
+            int ultimoIndice = tipoDocSelec.Count - 1;
+            tipoDocSelec[ultimoIndice] = tipoDocSelec[ultimoIndice].Replace(",", string.Empty);
+            string tipoDocString = string.Concat(tipoDocSelec);
+
+            // *** SQL QUERY ***
+            StringBuilder queryBuilder = new StringBuilder();
+            queryBuilder.Append(" SELECT NULL AS Cf, CONVERT(varchar, Data, 103) AS Data, TipoDoc, Serie, NumDoc, TipoEntidade, Entidade, Moeda, TotalDocumento");
+            queryBuilder.Append(" FROM CabecDoc");
+            queryBuilder.Append($" WHERE Data BETWEEN CONVERT(datetime, '{dataInicial}', 103) AND CONVERT(datetime, '{dataFinal}', 103)");
+            queryBuilder.Append($" AND TipoDoc IN ({tipoDocString})");
+            // Se f4_Cliente estiver nulo, busca todos os docs.
+            if (string.IsNullOrWhiteSpace(f4_Cliente.Text)) { queryBuilder.Append($" AND Entidade = {f4_Cliente.Text}"); }
+            queryBuilder.Append(" ORDER BY TipoDoc, NumDoc DESC;");
+
+            // Preencher PriGrelha com resultados da query. DataBind deve ser feito a uma StdBELista (retornado por BSO.Consulta)
+            priGrelha_Docs.LimpaGrelha();
+            priGrelha_Docs.DataBind(BSO.Consulta(queryBuilder.ToString()));
+        }
+
+        #endregion
+
     }
 }
