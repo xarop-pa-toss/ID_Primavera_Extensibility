3d8b5c2d36e5c1e17cf83d60bc2396f3f99e49c2
xarop-pa-toss <rpgdsantos@gmail.com>
2024-02-09 11:52:02 +0000

    Final conversão da FichaClientes em PP_Qualidade

--- PP_Extens/PP_Qualidade/Base/UiFichaClientes.cs
+++ PP_Extens/PP_Qualidade/Base/UiFichaClientes.cs
@@ -1,9 +1,23 @@
 using Primavera.Extensibility.Base.Editors;
+using Primavera.Extensibility.BusinessEntities.ExtensibilityService.EventArgs;
 
 
 namespace PP_Qualidade.Base
 {
     public class UiFichaClientes : FichaClientes
     {
+        public override void AntesDeGravar(ref bool Cancel, ExtensibilityEventArgs e)
+        {
+            base.AntesDeGravar(ref Cancel, e);
+
+            if (string.IsNullOrEmpty(Cliente.Morada)
+                || string.IsNullOrEmpty(Cliente.Localidade)
+                || string.IsNullOrEmpty(Cliente.Distrito))
+            {
+                Cancel = true;
+                PSO.MensagensDialogos.MostraErro("O cliente necessita de ter definida a Morada, Localidade e Distrito", StdPlatBS100.StdBSTipos.IconId.PRI_Exclama);
+            }
+
+        }
     }
 }
